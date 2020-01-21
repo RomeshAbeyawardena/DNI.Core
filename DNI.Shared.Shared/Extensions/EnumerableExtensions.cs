4740e0d91db800d63a61057b6a9f79df1eeaee1d
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2020-01-21 22:04:36 +0000

    Added enumerable extensions

---
+++ DNI.Shared.Shared/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNI.Shared.Shared.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<T> Append<T>(this IEnumerable<T> items, IEnumerable<T> newItems)
+        {
+            var itemList = new List<T>(items);
+            itemList.AddRange(newItems);
+            return itemList.ToArray();
+        }
+    }
+}
