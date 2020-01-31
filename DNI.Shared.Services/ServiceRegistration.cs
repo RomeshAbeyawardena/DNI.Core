da181321918dfe97dd260dbdadcc14b0d7672cf8
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2020-01-31 23:44:37 +0000

    Added cache factory

--- DNI.Shared.Services/ServiceRegistration.cs
+++ DNI.Shared.Services/ServiceRegistration.cs
@@ -1,9 +1,11 @@
 ﻿using DNI.Shared.Contracts;
 using DNI.Shared.Contracts.Convertors;
+using DNI.Shared.Contracts.Factories;
 using DNI.Shared.Contracts.Managers;
 using DNI.Shared.Contracts.Providers;
 using DNI.Shared.Contracts.Services;
 using DNI.Shared.Services.Convertors;
+using DNI.Shared.Services.Factories;
 using DNI.Shared.Services.Providers;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Internal;
@@ -28,6 +30,7 @@ namespace DNI.Shared.Services
                 .AddSingleton<IJsonWebTokenService, JsonWebTokenService>()
                 .AddSingleton<IMemoryStreamManager, MemoryStreamManager>()
                 .AddSingleton<ICryptographyProvider, CryptographyProvider>()
+                .AddSingleton<ICacheProviderFactory, DefaultCacheProviderFactory>()
                 .AddTransient<IMediatorService, MediatorService>();
         }
     }
