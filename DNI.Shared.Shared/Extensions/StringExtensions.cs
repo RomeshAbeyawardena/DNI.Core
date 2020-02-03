d84cf24386d0ce1382b298c3762c135fb0321ed5
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2020-02-03 15:17:46 +0000

    Added extensions

--- DNI.Shared.Shared/Extensions/StringExtensions.cs
+++ DNI.Shared.Shared/Extensions/StringExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Text;
+using System;
+using System.Linq;
 
 namespace DNI.Shared.Shared.Extensions
 {
@@ -8,6 +10,16 @@ namespace DNI.Shared.Shared.Extensions
         public static IEnumerable<byte> GetBytes(this string value, Encoding encoding)
         {
             return encoding.GetBytes(value);
+        }
+
+        public static string ToBase64String(this string value, Encoding encoding)
+        {
+            return Convert.ToBase64String(value.GetBytes(encoding).ToArray());
+        }
+
+        public static string FromBase64String(this string value, Encoding encoding)
+        {
+            return Convert.FromBase64String(value).GetString(encoding);
         }
 
         public static string DisplayIf(this string value, bool condition = true, string valueOnConditionFalse = default)
