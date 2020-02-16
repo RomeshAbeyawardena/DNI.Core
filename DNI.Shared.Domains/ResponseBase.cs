ab18301371b2b4ac0d25c13cf5cc2dcf7607acf8
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2020-02-16 19:09:18 +0000

    Added Deletion method

--- DNI.Shared.Domains/ResponseBase.cs
+++ DNI.Shared.Domains/ResponseBase.cs
@@ -7,9 +7,14 @@ using System.Threading.Tasks;
 
 namespace DNI.Shared.Domains
 {
-    public class ResponseBase
+    public abstract class ResponseBase
     {
         public bool IsSuccessful { get; set; }
         public IEnumerable<ValidationFailure> Errors { get; set; }
+    }
+
+    public abstract class ResponseBase<T> : ResponseBase
+    {
+        public T Result { get; set; }
     }
 }
