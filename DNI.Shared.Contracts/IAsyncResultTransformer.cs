e5184d8116884d296a3c3fe2c2fb4b7837aa493a
Anthony Abeyawardena <Anthony.Abeyawardena@connellsgroup.co.uk>
2020-03-04 12:36:22 +0000

    Added more methods to IAsyncResultTransformer

--- DNI.Shared.Contracts/IAsyncResultTransformer.cs
+++ DNI.Shared.Contracts/IAsyncResultTransformer.cs
@@ -1,18 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DNI.Shared.Contracts
 {
     public interface IAsyncResultTransformer<T>
     {
-        Task<IEnumerable<T>> ArrayAsync();
-        Task<IList<T>> ListAsync();
-        Task<T> FirstAsync();
-        Task<T> FirstOrDefaultAsync();
-        Task<T> SingleAsync();
-        Task<T> SingleOrDefaultAsync();
+        Task<IEnumerable<T>> ArrayAsync(CancellationToken cancellationToken);
+        Task<decimal?> SumAsync(Expression<Func<T, decimal?>> selector, CancellationToken cancellationToken);
+        Task<int> CountAsync(CancellationToken cancellationToken);
+        Task<long> LongCountAsync(CancellationToken cancellationToken);
+        Task<IList<T>> ListAsync(CancellationToken cancellationToken);
+        Task<T> FirstAsync(CancellationToken cancellationToken);
+        Task<T> FirstOrDefaultAsync(CancellationToken cancellationToken);
+        Task<T> SingleAsync(CancellationToken cancellationToken);
+        Task<T> SingleOrDefaultAsync(CancellationToken cancellationToken);
     }
 }
--- DNI.Shared.Services/DefaultAsyncResultTransformer.cs
+++ DNI.Shared.Services/DefaultAsyncResultTransformer.cs
@@ -3,7 +3,9 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DNI.Shared.Services
@@ -30,34 +32,49 @@ namespace DNI.Shared.Services
             _query = query;
         }
 
-        public async Task<IEnumerable<T>> ArrayAsync()
+        public async Task<IEnumerable<T>> ArrayAsync(CancellationToken cancellationToken)
         {
-            return await _query.ToArrayAsync();
+            return await _query.ToArrayAsync(cancellationToken);
         }
 
-        public async Task<T> FirstAsync()
+        public async Task<T> FirstAsync(CancellationToken cancellationToken)
         {
-            return await _query.FirstAsync();
+            return await _query.FirstAsync(cancellationToken);
         }
 
-        public async Task<T> FirstOrDefaultAsync()
+        public async Task<T> FirstOrDefaultAsync(CancellationToken cancellationToken)
         {
-            return await _query.FirstOrDefaultAsync();
+            return await _query.FirstOrDefaultAsync(cancellationToken);
         }
 
-        public async Task<IList<T>> ListAsync()
+        public async Task<IList<T>> ListAsync(CancellationToken cancellationToken)
         {
-            return await _query.ToListAsync();
+            return await _query.ToListAsync(cancellationToken);
         }
 
-        public async Task<T> SingleAsync()
+        public async Task<T> SingleAsync(CancellationToken cancellationToken)
         {
-            return await _query.SingleAsync();
+            return await _query.SingleAsync(cancellationToken);
         }
 
-        public async Task<T> SingleOrDefaultAsync()
+        public async Task<T> SingleOrDefaultAsync(CancellationToken cancellationToken)
         {
-            return await _query.SingleOrDefaultAsync();
+            return await _query.SingleOrDefaultAsync(cancellationToken);
+        }
+
+        public async Task<decimal?> SumAsync(Expression<Func<T,decimal?>> selector, CancellationToken cancellationToken)
+        {
+            return await _query.SumAsync(selector, cancellationToken);
+        }
+
+        public async Task<int> CountAsync(CancellationToken cancellationToken)
+        {
+            return await _query.CountAsync(cancellationToken);
+        }
+
+        public async Task<long> LongCountAsync(CancellationToken cancellationToken)
+        {
+            return await _query.LongCountAsync(cancellationToken);
         }
     }
 }
