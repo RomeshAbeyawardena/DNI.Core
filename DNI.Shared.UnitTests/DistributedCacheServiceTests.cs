41f4c5bc1273d3ed185ddd8f164077eb0b8b5d42
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2020-03-04 21:01:27 +0000

    Fixed tests

--- DNI.Shared.UnitTests/DistributedCacheServiceTests.cs
+++ DNI.Shared.UnitTests/DistributedCacheServiceTests.cs
@@ -85,7 +85,7 @@ namespace DNI.Shared.UnitTests
         }
 
         [Test]
-        public async Task Set_when_parameter_not_null_calls_deserialize()
+        public async Task Set_when_parameter_not_null_calls_Serialize()
         {
             var cancellationToken = CancellationToken.None;
             var testCacheModel = new TestCacheModel();
@@ -116,25 +116,33 @@ namespace DNI.Shared.UnitTests
 
         
         [Test]
-        public async Task Set_when_parameter_null_does_not_call_deserialize()
+        public async Task Set_when_parameter_null_does_not_call_Serialize()
         {
             var cancellationToken = CancellationToken.None;
-            var testCacheModel = new TestCacheModel();
-            var cachedByteResults = Array.Empty<byte>();
-            distributedCacheMock
-                .Setup(distributedCache => distributedCache.GetAsync(nameof(TestCacheModel), cancellationToken))
-                .Returns(Task.FromResult(cachedByteResults)).Verifiable();
+            TestCacheModel testCacheModel = default;
 
-            messagePackServiceMock.Setup(messagePackService => messagePackService
-                .Deserialise<TestCacheModel>(cachedByteResults, sut._messagePackOptions))
-                .Returns(Task.FromResult(default(TestCacheModel)))
+            var cachedByteResults = new byte[32];
+
+            Array.Fill<byte>(cachedByteResults, 255);
+
+            distributedCacheMock
+                .Setup(distributedCache => distributedCache.SetAsync(nameof(TestCacheModel),cachedByteResults, 
+                    It.IsAny<DistributedCacheEntryOptions>(), cancellationToken))
+                .Returns(Task.CompletedTask)
                 .Verifiable();
 
-            var result = await sut.Get<TestCacheModel>(nameof(TestCacheModel), cancellationToken);
-            distributedCacheMock.Verify(distributedCache => distributedCache
-                .GetAsync(nameof(TestCacheModel), cancellationToken), Times.Once);
+            messagePackServiceMock.Setup(messagePackService => messagePackService
+                .Serialise(testCacheModel, sut._messagePackOptions))
+                .Returns(Task.FromResult(cachedByteResults.AsEnumerable()))
+                .Verifiable();
+
+            await sut.Set(nameof(TestCacheModel), testCacheModel, cancellationToken);
+
+            distributedCacheMock.Verify(distributedCache => distributedCache.SetAsync(nameof(TestCacheModel),cachedByteResults, 
+                    It.IsAny<DistributedCacheEntryOptions>(), cancellationToken), Times.Never);
+
             messagePackServiceMock.Verify(messagePackService => messagePackService
-                .Deserialise<TestCacheModel>(cachedByteResults, sut._messagePackOptions), Times.Never);
+                .Serialise(testCacheModel, sut._messagePackOptions), Times.Never);
         }
     }
 }
