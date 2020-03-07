60bf2316e120c4f17b7eda5bfc3e833f8baaa2ff
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2020-03-07 17:47:08 +0000

    Added Describer helper

---
+++ DNI.Core.Contracts/IDescribe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNI.Core.Contracts
+{
+    public interface IDescribe
+    {
+        Action<ITypesDescriptor> Types { get; set; }
+        Action<IAssembliesDescriptor> Assemblies { get; set; }
+        IEnumerable<Type> DescribedTypes { get; }
+        IEnumerable<Assembly> DescribedAssemblies { get; }
+    }
+}
