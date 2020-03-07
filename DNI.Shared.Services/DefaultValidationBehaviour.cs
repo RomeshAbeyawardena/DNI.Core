9221f0637986b19ebbd069b933e55a4f39276e9d
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2020-03-07 09:07:24 +0000

    DefaultValidationBehaviour is now optional, and will only be used when availabe for a mediator request, otherwise ignored.

--- DNI.Shared.Services/DefaultValidationBehaviour.cs
+++ DNI.Shared.Services/DefaultValidationBehaviour.cs
@@ -8,7 +8,6 @@ using System.Threading.Tasks;
 namespace DNI.Shared.Services
 {
     public sealed class DefaultValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
-        where TResponse : ResponseBase
     {
         private readonly IValidator<TRequest> _validator;
 
@@ -18,19 +17,22 @@ namespace DNI.Shared.Services
             if(_validator == null)
                 return await next();
 
+            var response = Activator.CreateInstance<TResponse>();
+                
+            if(!(response is ResponseBase responseBase))
+                return await next();
+
             var result  = await _validator.ValidateAsync(request);
 
             if(result.IsValid)
                 return await next();
 
-            var response = Activator.CreateInstance<TResponse>();
-
-            response.Errors = result.Errors;
-            response.IsSuccessful = false;
+            responseBase.Errors = result.Errors;
+            responseBase.IsSuccessful = false;
             return response;
         }
 
-        public DefaultValidationBehaviour(IValidator<TRequest> validator)
+        public DefaultValidationBehaviour(IValidator<TRequest> validator = null)
         {
             _validator = validator;
         }
