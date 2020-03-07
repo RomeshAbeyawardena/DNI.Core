2148e828bde1c8f406fe436459bfabbae05addde
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2020-03-07 14:15:23 +0000

    Added encryption key

---
+++ DNI.Core.Domains/EncryptionKey.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNI.Core.Domains
+{
+    public class EncryptionKey
+    {
+        public string Salt { get; set; }
+        public string Password { get; set; }
+        public int Iterations { get; set; }
+        public string InitialVector { get; set; }
+    }
+}
