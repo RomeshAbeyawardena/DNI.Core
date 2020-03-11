54087d86ba50685a1509671518317a7bf6868d64
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2020-03-11 18:24:16 +0000

    Added intellisense comments

--- DNI.Core.Contracts/Providers/IMapperProvider.cs
+++ DNI.Core.Contracts/Providers/IMapperProvider.cs
@@ -2,9 +2,26 @@
 
 namespace DNI.Core.Contracts
 {
+    /// <summary>
+    /// Represents a mapper that encapsulates AutoMapper
+    /// </summary>
     public interface IMapperProvider
     {
+        /// <summary>
+        /// Maps TSource to a new instance of TDestination using AutoMapper
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TDestination"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
         TDestination Map<TSource, TDestination>(TSource source);
+        /// <summary>
+        /// Maps an array of TSource to a new array instance of TDestination using AutoMapper
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TDestination"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
         IEnumerable<TDestination> Map<TSource, TDestination>(IEnumerable<TSource> source);
     }
 }
