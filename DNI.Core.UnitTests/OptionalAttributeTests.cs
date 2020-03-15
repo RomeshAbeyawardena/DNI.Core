e23e6fe36347e18c4f795a776989cf8c66dc08a7
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2020-03-15 22:23:43 +0000

    Added optional attribute tests

--- DNI.Core.UnitTests/OptionalAttributeTests.cs
+++ DNI.Core.UnitTests/OptionalAttributeTests.cs
@@ -34,12 +34,42 @@ namespace DNI.Core.UnitTests
             Assert.False(Validator.TryValidateObject(testClass, validateContext, validationResults, true));
         }
 
+        [TestCase(0, 1, 0)]
+        [TestCase(1, null, 0)]
+        [TestCase(null, null, 1)]
+        [TestCase(null, 0, 1)]
+        public void Validation_should_pass(int aValue, int bValue, int cValue)
+        {
+            var testClass = new TestClass_int { A = aValue, B = bValue, C = cValue };
+            var validateContext = new ValidationContext(testClass);
+            var validationResults = new List<ValidationResult>();
+            Assert.True(Validator.TryValidateObject(testClass, validateContext, validationResults, true));
+        }
+
+        [TestCase(null, null, null)]
+        [TestCase(0, 0, 0)]
+        public void Validation_should_fail(int aValue, int bValue, int cValue)
+        {
+            var testClass = new TestClass_int { A = aValue, B = bValue, C = cValue };
+            var validateContext = new ValidationContext(testClass);
+            var validationResults = new List<ValidationResult>();
+            Assert.False(Validator.TryValidateObject(testClass, validateContext, validationResults, true));
+        }
+
         internal class TestClass
         {
             [Optional(nameof(B), nameof(C))]
             public string A { get; set; }
             public string B { get; set; }
             public string C { get; set; }
+        }
+
+        internal class TestClass_int
+        {
+            [Optional(nameof(B), nameof(C))]
+            public int A { get; set; }
+            public int B { get; set; }
+            public int C { get; set; }
         }
     }
 }
